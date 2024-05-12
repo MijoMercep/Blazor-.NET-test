fc833a827b4ca402038efacb42008a8b642c0ec5
MijoMercep <mijomercep1222@gmail.com>
2024-05-12 21:58:36 +0200

    test commit

---
+++ Proizvod.cs
@@ -0,0 +1,13 @@
+namespace Zadatak3
+{
+    class Proizvod
+    {
+        public string naziv, vrsta;
+        public double cijena;
+        
+
+        public Proizvod(){
+            vrsta="ostalo";
+        }
+    }
+}
